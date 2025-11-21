269918e9d5e7ad789859e8691566353fdd121068
raphael.gafurow <github@raphaelgafurow.de>
2025-11-21 18:08:18 +0100

    Add first version of console prototype with Program.cs and basic Pflanze class

---
+++ Implementierung/PflanzenpflegeKonsole/Pflanze.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PflanzenpflegeKonsole
+{
+    internal class Pflanze
+    {
+        public string Name { get; set; }
+        public string Art { get; set; }
+        public string Standort { get; set; }
+        public string Kaufdatum { get; set; }
+        public string Notizen { get; set; }
+    }
+}
---
+++ Implementierung/PflanzenpflegeKonsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PflanzenpflegeKonsole
+{
+    class Program
+    {
+        static List<Pflanze> pflanzen = new List<Pflanze>();
+
+        static void Main()
+        {
+           
+            Daten();
+
+            Console.WriteLine("Pflanzenpflege-Assistent");
+            
+
+            int i = 1;
+            foreach (var p in pflanzen)
+            {
+                Console.WriteLine($"{i}. {p.Name} – {p.Art} – {p.Standort} - {p.Kaufdatum} - {p.Notizen}");
+                i++;
+            }
+
+            
+        }
+
+        static void Daten()
+        {
+            pflanzen.Add(new Pflanze
+            {
+                Name = "Ficus Benjamina",
+                Art = "Ficus",
+                Standort = "Wohnzimmer",
+                Kaufdatum = "2024-05-10",
+                Notizen = "Mag helles, indirektes Licht"
+            });
+
+            pflanzen.Add(new Pflanze
+            {
+                Name = "Aloe Vera",
+                Art = "Aloe",
+                Standort = "Bad",
+                Kaufdatum = "2022-03-02",
+                Notizen = "Wenig gießen"
+            });
+        }
+    }
+}
