63fa7dbd21b19eaf97f21c4ea77550ff6ff92a71
raphael.gafurow <github@raphaelgafurow.de>
2026-02-22 20:14:48 +0100

    Configure application startup and initialize database

--- Implementierung/PflanzenpflegeAvalonia/App.axaml.cs
+++ Implementierung/PflanzenpflegeAvalonia/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using PflanzenpflegeAvalonia.Data;
 
 namespace PflanzenpflegeAvalonia;
 
@@ -13,6 +14,8 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        AppDbContext.EnsureCreatedAndSeeded();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
